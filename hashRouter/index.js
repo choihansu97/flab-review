3849253034401acee4579a3f47673a26d1c56eda
choihansu97 <jjpp9712@naver.com>
2023-04-03 16:46:23 +0900

    feat : 불필요한 파일 정리 및 gitignore 추가

---
+++ hashRouter/index.js
@@ -0,0 +1,16 @@
+import './hashRouter/css/style.css';
+import about from './about.html';
+import CreateRoute from './router.js'
+
+const router = CreateRoute();
+const container = document.querySelector('#hash-app');
+
+const page = {
+    home: () => container.innerText = '홈입니다.',
+    about: () => container.innerHTML = about,
+}
+
+router.addRoute('', page.home)
+.addRoute('#/', page.home)
+.addRoute('#/about', page.about)
+.start()
